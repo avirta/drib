ae0cc0aeabdf37ce061565a9903296ee782b57a5
avirta <vodoleeva11@mail.ru>
2013-03-27 12:17:29 +0200

    Update fraction.cpp

--- fraction.cpp
+++ fraction.cpp
@@ -5,6 +5,7 @@ class Fraction
 {
     int numerator; // чисельник 
     int denomerator; // знаменник
+     int integer; // ціле число
 public:
     int GetNumerator() { return numerator; }
     void SetNumerator(int num) { numerator=num; }
@@ -17,12 +18,14 @@ public:
         numerator = num;
         denomerator = denom;
     }
-  Fraction operator* (const Fraction &frac);// & означає, що нам потрібне посилання, а не копія об'єкту
-    Fraction operator/ (const Fraction &frac);
+    Fraction operator* (const Fraction &frac);// & означає, що нам потрібне посилання, а не копія об'єкту, 
+    Fraction operator/ (const Fraction &frac);// const, що об'єк за цим посиланням змінювати не можна
     Fraction operator- (const Fraction &frac);
     Fraction operator+ (const Fraction &frac);
     void ReduceByGreatestDenominator();//найбільший спільний знаменник
     int Fraction::CommonDenominator(const Fraction &frac);//спільний знаменник
+    int Fraction:: CommonToImproperFraction(const Fraction &frac); // зведення до неправильного дробу
+    int Fraction:: CommonToProperFraction(const Fraction &frac); // зведення до правильного дробу
 };
 Fraction Fraction::operator* (const Fraction &frac)
 {
@@ -31,4 +34,32 @@ Fraction Fraction::operator* (const Fraction &frac)
 Fraction Fraction::operator/ (const Fraction &frac)
 {
     return Fraction(numerator * frac.denomerator, denomerator * frac.numerator);
+}
+Fraction Fraction::operator- (const Fraction &frac)
+{
+    int comDenom = CommonDenominator(frac);
+    int num1 = comDenom / denomerator * numerator;
+    int num2 = comDenom / frac.denomerator * frac.numerator;
+    return Fraction(num1 - num2, comDenom);
+}
+Fraction Fraction::operator+ (const Fraction &frac)
+{
+    int comDenom = CommonDenominator(frac);
+    int num1 = comDenom / denomerator * numerator;
+    int num2 = comDenom / frac.denomerator * frac.numerator;
+    return Fraction(num1 + num2, comDenom);
+}
+void Fraction::ReduceByGreatestDenominator()
+{
+    int y = denomerator;
+    int x = numerator;
+    int t;       
+    while (y!=0)
+    {
+        t = y;
+        y = x % y;
+        x = t;
+    }
+    numerator /= x;
+    denomerator /= x;
 }
